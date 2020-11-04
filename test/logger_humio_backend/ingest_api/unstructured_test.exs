103b131c25159be37aab474e4f313b891d95f52d
Andreas Kasprzok <andreas@blockfi.com>
2020-11-04 11:24:44 -0800

    Structured and unstructured tests are working

--- test/logger_humio_backend/ingest_api/unstructured_test.exs
+++ test/logger_humio_backend/ingest_api/unstructured_test.exs
@@ -1,9 +1,11 @@
 defmodule Logger.Backend.Humio.IngestApi.UnstructuredTest do
   use ExUnit.Case, async: false
-  require Logger
 
-  alias Logger.Backend.Humio.IngestApi
-  alias Logger.Backend.Humio.Client
+  import Mox
+
+  alias Logger.Backend.Humio.{Client, IngestApi}
+
+  require Logger
 
   @backend {Logger.Backend.Humio, :test}
   Logger.add_backend(@backend)
@@ -19,10 +21,20 @@ defmodule Logger.Backend.Humio.IngestApi.UnstructuredTest do
     "env" => "dev"
   }
 
+  @happy_result {:ok, %{status: 200, body: "somebody"}}
+
   defp smoke_test_config(_context) do
+    set_mox_global()
+    parent = self()
+    ref = make_ref()
+    expect(Client.Mock, :send, fn request ->
+      send(parent, {ref, request})
+      @happy_result
+    end)
+
     config(
       ingest_api: IngestApi.Unstructured,
-      client: Client.Test,
+      client: Client.Mock,
       host: @base_url,
       format: "$message",
       token: @token,
@@ -31,14 +43,21 @@ defmodule Logger.Backend.Humio.IngestApi.UnstructuredTest do
       tags: @tags
     )
 
-    Client.Test.start_link(self())
-    :ok
+    {:ok, %{ref: ref}}
   end
 
   defp no_tags_or_fields_config(_context) do
+    set_mox_global()
+    parent = self()
+    ref = make_ref()
+    expect(Client.Mock, :send, fn request ->
+      send(parent, {ref, request})
+      @happy_result
+    end)
+
     config(
       ingest_api: IngestApi.Unstructured,
-      client: Client.Test,
+      client: Client.Mock,
       host: @base_url,
       format: "$message",
       token: @token,
@@ -47,37 +66,38 @@ defmodule Logger.Backend.Humio.IngestApi.UnstructuredTest do
       tags: %{}
     )
 
-    Client.Test.start_link(self())
-    :ok
+    {:ok, %{ref: ref}}
   end
 
   describe "smoke tests" do
     setup [:smoke_test_config]
 
-    test "Send payload successfully" do
+    test "Send payload successfully", %{ref: ref} do
       message = "message"
       Logger.info(message)
 
       expected_body = Jason.encode!([%{fields: @fields, tags: @tags, messages: [message]}])
 
       assert_receive(
-        {:send, %{body: ^expected_body, base_url: @base_url, path: @path, headers: @headers}}
+        {^ref, %{body: ^expected_body, base_url: @base_url, path: @path, headers: @headers}}
       )
+      verify!()
     end
   end
 
   describe "no tags or fields" do
     setup [:no_tags_or_fields_config]
 
-    test "neither are present in payload" do
+    test "neither are present in payload", %{ref: ref} do
       message = "message"
       Logger.info(message)
 
-      assert_receive({:send, %{body: body, base_url: @base_url, path: @path, headers: @headers}})
+      assert_receive({^ref, %{body: body, base_url: @base_url, path: @path, headers: @headers}})
 
       [decoded_body] = Jason.decode!(body)
       refute Map.has_key?(decoded_body, "tags")
       refute Map.has_key?(decoded_body, "fields")
+      verify!()
     end
   end
 
